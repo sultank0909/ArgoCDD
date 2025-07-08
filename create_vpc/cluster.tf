70f125084877eb447df0174c321fb37ea39ea21b
arfeoktistov <arrfeoktistov@gmail.com>
2025-07-08 11:04:58 +0600

    ready argo cd

---
+++ create_vpc/cluster.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+
+# Создание VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Создание Интернет-шлюза
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Создание таблицы маршрутов для public
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Создание public подсетей
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+# Ассоциация public таблицы маршрутов с public подсетями
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Создание private подсетей
+resource "aws_subnet" "private" {
+  count = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index + 2)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index}"
+  }
+}
+
+# Создание NAT-шлюза для private подсетей
+resource "aws_eip" "nat" {
+  count  = 1
+  domain = "vpc"
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat[0].id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# Создание таблицы маршрутов для private
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# Ассоциация private таблицы маршрутов с private подсетями
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+# Получение доступных зон
+data "aws_availability_zones" "available" {
+  state = "available"
+}
